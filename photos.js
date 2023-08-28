d2be212648de70f3d86de231bc5516f347035272
Braden <fish0859@gmail.com>
2023-08-28 14:05:30 -0600

    first commit

---
+++ photos.js
@@ -0,0 +1,17 @@
+export const photoList = [{
+    id: 1,
+    title: "10 Prominent Website Design Mistakes",
+    author: "Kennadee",
+    date: "05/2020",
+    imageUrl: "kitty.png",
+    imageAlt: "10 Prominent ",
+    snippet: "Nowadays."},
+    {
+    id: 2,
+    title: "10 Prominent Website Design Mistakes",
+    author: "Kennadee",
+    date: "05/2020",
+    imageUrl: "kitty.png",
+    imageAlt: "10 Prominent ",
+    snippet: "Nowadays.",
+}];
